8482125f87ff347b0b4817b5e5d27fd4c105230a
Marco Pestrin <pestrinmarco@gmail.com>
2019-05-26 11:15:59 +1000

    created first simple view in html

--- src/components/lift.js
+++ src/components/lift.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
-import classes from './lift.css';
+import './lift.css';
 
 let lift = ({onClick, text}) => (
-    <div>
-        <p>here!</p>
+    <div className="Skyscraper">
+
+        <div className="Level">4</div>
+        <button className="up" onclick="callUp()">UP!</button>
+        <button className="down" onclick="callDown()">DOWN!</button>
+
+        <div className="Level">3</div>
+        <button className="up">UP!</button>
+        <button className="down">DOWN!</button>
+
+        <div className="Level">2</div>
+        <button className="up">UP!</button>
+        <button className="down">DOWN!</button>
+
+        <div className="Level">1</div>
+        <button className="up">UP!</button>
+        <button className="down">DOWN!</button>
+
     </div>
 );
 
 lift.propTypes = {
-    onClick: PropTypes.func.isRequired,
+   /* onClick: PropTypes.func.isRequired,
     text:PropTypes.string.isRequired
+    */
 }
 const liftComponent = connect()(lift)
 
